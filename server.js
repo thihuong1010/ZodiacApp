d4ae562f2ca630e7e6d0fa8b28e18a0aef0218e8
Huong Nguyen <huongnguyen10102@gmail.com>
2021-09-16 13:54:12 +0300

    add frontend folder

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// express module and use the dot to access to open Express module
+// __dirname : current project
+app.use(express.static(__dirname + '/frontend'));
+
 const port = 3000;
 
 app.listen(port, () => {
-    console.log('Server is running on port' + port);
+    console.log('Server is running by nodemon on port ' + port);
 });
